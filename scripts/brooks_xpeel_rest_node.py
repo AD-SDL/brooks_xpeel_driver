6c448bf8af0cfde8b819691aa45058ab73143c7a
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2024-03-01 11:20:22 -0600

    typo fix

--- scripts/brooks_xpeel_rest_node.py
+++ scripts/brooks_xpeel_rest_node.py
@@ -1,4 +1,5 @@
 """The server that takes incoming WEI flow requests from the experiment application"""
+
 import json
 from argparse import ArgumentParser
 from contextlib import asynccontextmanager
@@ -7,10 +8,11 @@ from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import JSONResponse
 
 from brooks_xpeel_driver.brooks_xpeel_driver import BROOKS_PEELER_DRIVER
-from wei.core.data_classes import ModuleAbout, ModuleActions
+from wei.core.data_classes import ModuleAbout, ModuleAction
 
 global peeler, state
 device = ""
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -68,16 +70,13 @@ async def about():
             ModuleAction(
                 name="peel",
                 description="This action peels a plate that is currenly in the peeling station.",
-                args=[
-                    
-                ],
+                args=[],
             )
         ],
         resource_pools=[],
     )
     return JSONResponse(content=about.model_dump(mode="json"))
 
-    
 
 @app.get("/resources")
 async def resources():
@@ -122,10 +121,21 @@ def do_action(
 
 if __name__ == "__main__":
     import uvicorn
+
     parser = ArgumentParser()
-    parser.add_argument("--host", type=str, default="0.0.0.0", help="Hostname that the REST API will be accessible on")
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="0.0.0.0",
+        help="Hostname that the REST API will be accessible on",
+    )
     parser.add_argument("--port", type=int, default=2001)
-    parser.add_argument("--device", type=str, default="/dev/ttyUSB1", help="Serial device for communicating with the device")
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="/dev/ttyUSB1",
+        help="Serial device for communicating with the device",
+    )
     args = parser.parse_args()
     device = args.device
 
